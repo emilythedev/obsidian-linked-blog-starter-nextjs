740c0e9dd7e708e27431e40cafa6e3f2a0829e59
Emily L <emilythedev@gmail.com>
2024-08-09 19:40:51 +0900

    hide footer

--- components/misc/layout.tsx
+++ components/misc/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Footer from './footer'
 import Header from './header'
 
 type Props = {
@@ -13,7 +12,7 @@ const Layout = ({ children }: Props) => {
       <main className="grow">
         {children}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
