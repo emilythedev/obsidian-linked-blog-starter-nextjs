3ec911e7bb1dcd284096b238f3b08d35261fc9c9
Emily L <emilythedev@gmail.com>
2024-08-14 21:07:17 +0900

    update letter spacing

--- components/misc/layout.tsx
+++ components/misc/layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+    <div className="flex flex-col min-h-screen overflow-hidden tracking-wide">
       <Header />
       <main className="grow">
         {children}
